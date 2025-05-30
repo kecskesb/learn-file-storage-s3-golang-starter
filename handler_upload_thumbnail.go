3698b3159ee5bb4edadcdae0a79ae2f694165344
Balazs Kecskes <kecskesb@gmail.com>
2025-05-31 01:28:22 +0200

    Updates in handlerUploadThumbnail()

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +34,92 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	mimeType, _, err := mime.ParseMediaType(r.Header.Get("Content-Type"))
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Invalid Content-Type header", err)
+		return
+	}
+	if mimeType != "image/jpeg" && mimeType != "image/png" {
+		respondWithError(w, http.StatusUnsupportedMediaType, "Unsupported media type", nil)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	const maxMemory = 10 << 20
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't parse form", err)
+		return
+	}
+	data, headers, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't get thumbnail file", err)
+		return
+	}
+	defer data.Close()
+	mediaType := headers.Header.Get("Content-Type")
+	if mediaType == "" {
+		respondWithError(w, http.StatusBadRequest, "Content-Type header is required", nil)
+		return
+	}
+	imageData, err := io.ReadAll(data)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't read thumbnail data", err)
+		return
+	}
+	videoMeta, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get video metadata", err)
+		return
+	}
+	if videoMeta.UserID != userID {
+		respondWithError(w, http.StatusForbidden, "You are not allowed to upload a thumbnail for this video", nil)
+		return
+	}
+	newThumbnail := thumbnail{
+		data:      imageData,
+		mediaType: mediaType,
+	}
+	videoThumbnails[videoID] = newThumbnail
+
+	//newThumbnailUrl := fmt.Sprintf("http://localhost:8091/api/thumbnails/%s", videoID.String())
+	//videoMeta.ThumbnailURL = &newThumbnailUrl
+	//err = cfg.db.UpdateVideo(videoMeta)
+	//if err != nil {
+	//	respondWithError(w, http.StatusInternalServerError, "Couldn't update video metadata with thumbnail URL", err)
+	//	return
+	//}
+
+	//imageBase64 := base64.StdEncoding.EncodeToString(imageData)
+	//dataUrl := fmt.Sprintf("data:%s;base64,%s", mediaType, imageBase64)
+	//videoMeta.ThumbnailURL = &dataUrl
+	//err = cfg.db.UpdateVideo(videoMeta)
+	//if err != nil {
+	//	respondWithError(w, http.StatusInternalServerError, "Couldn't update video metadata with data URL", err)
+	//	return
+	//}
+
+	fileExtension := mediaType[strings.Index(mediaType, "/")+1:]
+	filePath := filepath.Join(cfg.assetsRoot, fmt.Sprintf("%s.%s", videoID, fileExtension))
+	fileHandle, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't create thumbnail file", err)
+		return
+	}
+	_, err = io.Copy(fileHandle, bytes.NewReader(imageData))
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't write thumbnail data to file", err)
+		return
+	}
+
+	newThumbnailUrl := fmt.Sprintf("http://localhost:8091/%s", filePath)
+	videoMeta.ThumbnailURL = &newThumbnailUrl
+	err = cfg.db.UpdateVideo(videoMeta)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Couldn't update video metadata with thumbnail URL", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, videoMeta)
 }
